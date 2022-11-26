e86c09cffb4ebabbbcd6968c96bfc8688c0e5ab6
232026 <232026@WINMAC22>
2022-11-26 12:59:41 +0300

    Добавьте файлы проекта.

---
+++ Проверочная/Components/PartialClass/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проверочная.Components
+{
+    public partial class Employee
+    {
+        public string StrFullName
+        {
+            get
+            {
+                return $"{LastName} {FirstName}";
+            }
+        }
+
+        public string StrFullAddress
+        {
+            get
+            {
+                return $"{Address}, {City}, {State} {Zipcode}";
+            }
+        }
+    }
+}
---
+++ Проверочная/Pages/List2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Проверочная.Components;
+
+namespace Проверочная.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для List2.xaml
+    /// </summary>
+    public partial class List2 : Page
+    {
+        public List2()
+        {
+            InitializeComponent();
+            EmployeeList.ItemsSource = DBConnect.Entities.Employee.ToList();
+        }
+    }
+}
